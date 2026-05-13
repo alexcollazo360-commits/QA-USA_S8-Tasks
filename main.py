0b142ca075bd051890d5ecb5e46d4943556dec41
alexc <alexcollazo360@gmail.com>
2026-05-12 22:04:02 -0400

    Sprint 8 Project updated files

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import pytest
 from selenium import webdriver
-from selenium.webdriver import DesiredCapabilities
 import data
 import helpers
 from pages import UrbanRoutesPage
@@ -10,15 +8,13 @@ class TestUrbanRoutes:
 
     @classmethod
     def setup_class(cls):
-        # do not modify - we need additional logging enabled in order to retrieve phone confirmation code
         from selenium.webdriver import DesiredCapabilities
         capabilities = DesiredCapabilities.CHROME
         capabilities["goog:loggingPrefs"] = {'performance': 'ALL'}
         cls.driver = webdriver.Chrome()
 
-        # Your URL reachability check should be indented here
-        if helpers.is_url_reachable(data.urban_routes_url):
-            cls.driver.get(data.urban_routes_url)
+        if helpers.is_url_reachable(data.URBAN_ROUTES_URL):
+            cls.driver.get(data.URBAN_ROUTES_URL)
 
     @classmethod
     def teardown_class(cls):
@@ -26,86 +22,79 @@ class TestUrbanRoutes:
 
     def test_select_supportive_plan(self):
         routes_page = UrbanRoutesPage(self.driver)
-        routes_page.set_route()
+        routes_page.set_from_address(data.ADDRESS_FROM)
+        routes_page.set_to_address(data.ADDRESS_TO)
         routes_page.click_call_taxi_button()
-        routes_page.select_supportive_plan()
-        selected_plan = routes_page.get_selected_plan_text()
-        assert "Supportive" in selected_plan
+        routes_page.selecting_supportive_plan()
+        assert routes_page.active_selection_value == 'Supportive'
 
     def test_set_route(self):
         routes_page = UrbanRoutesPage(self.driver)
-        routes_page.set_from(data.ADDRESS_FROM)
-        routes_page.set_to(data.ADDRESS_TO)
+        routes_page.set_from_address(data.ADDRESS_FROM)
+        routes_page.set_to_address(data.ADDRESS_TO)
 
-    def test_select_comfort_tariff(self):
-        routes_page = UrbanRoutesPage(self.driver)
-        routes_page.set_route()
-        routes_page.click_call_taxi_button()
-        routes_page.select_supportive_plan()
-        selected_plan = routes_page.get_selected_plan_text()
-        assert "Supportive" in selected_plan
 
     def test_fill_in_phone_number(self):
         routes_page = UrbanRoutesPage(self.driver)
-        routes_page.set_route()
+        routes_page.set_from_address(data.ADDRESS_FROM)
+        routes_page.set_to_address(data.ADDRESS_TO)
         routes_page.click_call_taxi_button()
-        routes_page.select_supportive_plan()
-        routes_page.click_phone_number_field()
-        routes_page.enter_phone_number(data.PHONE_NUMBER)
-        routes_page.click_next_button()
+        routes_page.selecting_supportive_plan()
+        routes_page.filling_phone_number()
         code = helpers.retrieve_phone_code(self.driver)
-        routes_page.enter_phone_code(code)
-        routes_page.click_confirm_button()
+        routes_page.phone_code(code)
+        routes_page.final_taxi_order()
 
     def test_add_credit_card(self):
         routes_page = UrbanRoutesPage(self.driver)
-        routes_page.set_route()
+        routes_page.set_from_address(data.ADDRESS_FROM)
+        routes_page.set_to_address(data.ADDRESS_TO)
         routes_page.click_call_taxi_button()
-        routes_page.select_supportive_plan()
-        routes_page.click_payment_method()
-        routes_page.click_add_card()
-        routes_page.enter_card_number(data.CARD_NUMBER)
-        routes_page.enter_card_code(data.CARD_CODE)
-        routes_page.click_outside_modal()
+        routes_page.selecting_supportive_plan()
+        routes_page.adding_credit_card()
         routes_page.click_link_button()
 
     def test_write_message_for_driver(self):
         routes_page = UrbanRoutesPage(self.driver)
-        routes_page.set_route()
+        routes_page.set_from_address(data.ADDRESS_FROM)
+        routes_page.set_to_address(data.ADDRESS_TO)
         routes_page.click_call_taxi_button()
-        routes_page.select_supportive_plan()
-        routes_page.enter_message_for_driver(data.MESSAGE_FOR_DRIVER)
-        message = routes_page.get_message_for_driver()
+        routes_page.selecting_supportive_plan()
+        routes_page.writing_driver_comment()
+        message = routes_page.writing_driver_comment()
         assert message == data.MESSAGE_FOR_DRIVER
 
     def test_order_blanket_and_handkerchiefs(self):
         routes_page = UrbanRoutesPage(self.driver)
-        routes_page.set_route()
+        routes_page.set_from_address(data.ADDRESS_FROM)
+        routes_page.set_to_address(data.ADDRESS_TO)
         routes_page.click_call_taxi_button()
-        routes_page.select_supportive_plan()
-        routes_page.click_blanket_and_handkerchiefs()
-        assert routes_page.is_blanket_and_handkerchiefs_selected()
+        routes_page.selecting_supportive_plan()
+        routes_page.order_blanket_and_handkerchiefs()
+        assert routes_page.order_blanket_and_handkerchiefs()
 
     def test_order_2_ice_creams(self):
         routes_page = UrbanRoutesPage(self.driver)
-        routes_page.set_route()
-        routes_page.select_supportive_plan()
-        routes_page.set_phone_number()
-        routes_page.add_credit_card()
-        routes_page.write_message_for_driver()
-        routes_page.select_blanket_and_handkerchiefs()
+        routes_page.set_from_address(data.ADDRESS_FROM)
+        routes_page.set_to_address(data.ADDRESS_TO)
+        routes_page.selecting_supportive_plan()
+        routes_page.filling_phone_number()
+        routes_page.adding_credit_card()
+        routes_page.writing_driver_comment()
+        routes_page.order_blanket_and_handkerchiefs()
         for number in range(2):
             routes_page.ordering_ice_cream()
-        assert routes_page.get_ice_cream_counter() == "2"
+        assert routes_page.ordering_ice_cream() == "2"
 
     def test_the_car_search_modal_appears(self):
         routes_page = UrbanRoutesPage(self.driver)
-        routes_page.set_route()
-        routes_page.select_supportive_plan()
-        routes_page.set_phone_number()
-        routes_page.add_credit_card()
-        routes_page.write_message_for_driver()
-        routes_page.select_blanket_and_handkerchiefs()
-        routes_page.order_2_ice_creams()
-        routes_page.click_order_button()
-        assert routes_page.is_car_search_modal_visible()
+        routes_page.set_from_address(data.ADDRESS_FROM)
+        routes_page.set_to_address(data.ADDRESS_TO)
+        routes_page.selecting_supportive_plan()
+        routes_page.filling_phone_number()
+        routes_page.adding_credit_card()
+        routes_page.writing_driver_comment()
+        routes_page.order_blanket_and_handkerchiefs()
+        routes_page.ordering_ice_cream()
+        routes_page.final_taxi_order()
+        assert routes_page.final_taxi_order()
--- pages.py
+++ pages.py
@@ -1,147 +1,111 @@
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.wait import WebDriverWait
-from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.support import expected_conditions as ec
 from selenium.webdriver.common.keys import Keys
 import data
 
 class UrbanRoutesPage:
-    url = "https://cnt-a9a99bf8-8ce5-43a5-ad78-8535a14e2439.containerhub.tripleten-services.com"
     # Locators
     from_field = (By.ID, "from")
     to_field = (By.ID, "to")
     call_taxi_button = (By.ID, "button")
+    supportive_plan_option = (By.XPATH, "//div[contains(text(), 'Supportive')])")
+    payment_method = (By.XPATH, "//*[@id='root']/div/div[2]/div[2]/div[1]/div[2]/div[3]")
+    add_card_button = (By.XPATH, "//div[text()='Add card']")
+    phone_number_field = (By.XPATH, "//*[@id='root']/div/div[3]/div[3]/div[2]/div[2]/div[1]")
+    confirmation_field = (By.ID, "code")
+    card_number_field = (By.ID, "number")
+    card_code_field = (By.ID, "code")
+    link_button = (By.XPATH, "//button[text()='Link']")
+    comment_field = (By.ID, "comment")
+    add_order_blanket_and_handkerchiefs_button = (By.XPATH,
+                                                  '//*[@id="root"]/div/div[3]/div[3]/div[2]/div[2]/div[4]/div[2]/div[1]/div/div[2]/div/span')
+    ordering_ice_cream_button = (By.XPATH,
+                                 '//*[@id="root"]/div/div[3]/div[3]/div[2]/div[2]/div[4]/div[2]/div[3]/div/div[2]/div[1]/div/div[2]/div/div[3]')
+    ice_cream_counter = (By.XPATH,
+                         "//div[contains(text(), 'Ice cream')]/..//div[contains(@class, 'counter')]")
+    final_taxi_order_button = (By.XPATH, '//*[@id="root"]/div/div[3]/div[4]/button')
+    active_plan = (By.XPATH, '//div[@class="tcard active"] //div[@class="tcard-title"]')
+    phone_code = (By.ID,"code")
+
+
 
     def __init__(self, driver):
         self.driver = driver
 
-    def set_from_address(self):
-        from_field = (By.ID, "from")
-        self.driver.find_element(*from_field).send_keys("East 2nd Street, 601")
+    def click_link_button(self):
+        self.driver.find_element(*self.link_button).click()
 
-    def set_to_address(self):
-        to_field = (By.ID, "to")
-        self.driver.find_element(*to_field).send_keys("1300 1st St")
+
+    def set_from_address(self,address):
+        self.driver.find_element(*self.from_field).send_keys(address)
+
+    def set_to_address(self,address):
+        self.driver.find_element(*self.to_field).send_keys(address)
 
     def click_call_taxi_button(self):
-        call_taxi_button = (By.XPATH, "//button[text()='Call a taxi']")
-        self.driver.find_element(*call_taxi_button).click()
+        self.driver.find_element(*self.call_taxi_button).click()
+
+    def input_phone_code(self,code):
+        self.driver.find_element(*self.phone_code).click()
 
     def selecting_supportive_plan(self):
-        supportive_plan_option = self.driver.find_element(By.XPATH, "//div[contains(text(), 'Supportive')])")
-        supportive_plan_option.click()
-        assert active_selection_value.get_property('class') == 'tcard active'
+        self.driver.find_element(*self.supportive_plan_option).click()
 
     def filling_phone_number(self):
-        from data import phone_number
+        from data import PHONE_NUMBER
         from helpers import retrieve_phone_code
-
-        phone_number_field = (By.XPATH, "//*[@id='root']/div/div[3]/div[3]/div[2]/div[2]/div[1]")
-        self.driver.find_element(*phone_number_field).click()
-        self.driver.find_element(*phone_number_field).send_keys(phone_number)
-
-        # Retrieve SMS verification code
-        code = retrieve_phone_code()
-
-        # Enter the code into confirmation field
-        confirmation_field = (By.ID, "code")  # You'll need to find the correct locator
-        self.driver.find_element(*confirmation_field).send_keys(code)
-
-        # Assert that the phone number is properly reflected
-        displayed_number = self.driver.find_element(*phone_number_field).get_attribute("value")
-        assert displayed_number == phone_number
+        self.driver.find_element(*self.phone_number_field).click()
+        self.driver.find_element(*self.phone_number_field).send_keys(PHONE_NUMBER)
+        code = retrieve_phone_code(self)
+        self.driver.find_element(*self.confirmation_field).send_keys(code)
+        displayed_number = self.driver.find_element(*self.phone_number_field).get_attribute("value")
+        assert displayed_number == PHONE_NUMBER
 
 
-    def Adding_credit_card(self):
-        from data import card_number, card_code
-
-        # Click on Payment Method
-        payment_method = (By.XPATH, "//*[@id='root']/div/div[2]/div[2]/div[1]/div[2]/div[3]")
-        self.driver.find_element(*payment_method).click()
-
-        # Click on Add Card
-        add_card_button = (By.XPATH, "//div[text()='Add card']")
-        self.driver.find_element(*add_card_button).click()
-
-        # Enter valid Card Number
-        card_number_field = (By.ID, "number")  # You need to find the correct locator
-        self.driver.find_element(*card_number_field).send_keys(card_number)
-
-        # Enter Card Code
-        card_code_field = (By.ID, "code")  # You need to find the correct locator
-        self.driver.find_element(*card_code_field).send_keys(card_code)
-
-        # Use TAB to change focus from Code field
+    def adding_credit_card(self):
+        from data import CARD_NUMBER, CARD_CODE
+        self.driver.find_element(*self.payment_method).click()
+        self.driver.find_element(*self.add_card_button).click()
+        self.driver.find_element(*self.card_number_field).send_keys(CARD_NUMBER)
+        self.driver.find_element(*self.card_code_field).send_keys(CARD_CODE)
         from selenium.webdriver.common.keys import Keys
-        self.driver.find_element(*card_code_field).send_keys(Keys.TAB)
-
-        # Wait for Link button to become clickable
-        from selenium.webdriver.support.ui import WebDriverWait
-        from selenium.webdriver.support import expected_conditions as EC
-
-        link_button = (By.XPATH, "//button[text()='Link']")
+        self.driver.find_element(*self.card_code_field).send_keys(Keys.TAB)
         wait = WebDriverWait(self.driver, 10)
-        wait.until(EC.element_to_be_clickable(link_button))
-
-        # Click Link button
-        self.driver.find_element(*link_button).click()
-
-        # Assert that payment method changed to "Card"
-        payment_method_text = self.driver.find_element(*payment_method).text
+        wait.until(ec.element_to_be_clickable(*self.link_button))
+        self.driver.find_element(*self.link_button).click()
+        payment_method_text = self.driver.find_element(*self.payment_method).text
         assert "Card" in payment_method_text
 
 
-    def Writing_driver_comment(self):
-        from data import message_for_driver
-
-        # Locate the comment input field
-        comment_field = (By.ID, "comment")  # You need to find the correct locator
-
-        # Click on the comment field and enter the custom message
-        self.driver.find_element(*comment_field).click()
-        self.driver.find_element(*comment_field).send_keys(message_for_driver)
-
-        # Assert that the message is stored correctly
-        entered_text = self.driver.find_element(*comment_field).get_attribute("value")
-        assert entered_text == message_for_driver
+    def writing_driver_comment(self):
+        from data import MESSAGE_FOR_DRIVER
+        self.driver.find_element(*self.comment_field).click()
+        self.driver.find_element(*self.comment_field).send_keys(MESSAGE_FOR_DRIVER)
+        entered_text = self.driver.find_element(*self.comment_field).get_attribute("value")
+        assert entered_text == MESSAGE_FOR_DRIVER
 
 
     def order_blanket_and_handkerchiefs(self):
-        # Click on the "Add Blanket and Handkerchiefs" slider
-        add_order_blanket_and_handkerchiefs_button = (By.XPATH,
-                                                  '//*[@id="root"]/div/div[3]/div[3]/div[2]/div[2]/div[4]/div[2]/div[1]/div/div[2]/div/span')
-        self.driver.find_element(*add_order_blanket_and_handkerchiefs_button).click()
-
-        # Verify that the selection is confirmed using the correct assertion
-        blanket_slider = self.driver.find_element(*add_order_blanket_and_handkerchiefs_button)
+        self.driver.find_element(*self.add_order_blanket_and_handkerchiefs_button).click()
+        blanket_slider = self.driver.find_element(*self.add_order_blanket_and_handkerchiefs_button)
         is_checked = blanket_slider.get_property('checked')
         assert is_checked == True
 
 
     def ordering_ice_cream(self):
-        # Locate the ice cream plus button
-        ordering_ice_cream_button = (By.XPATH,
-                                 '//*[@id="root"]/div/div[3]/div[3]/div[2]/div[2]/div[4]/div[2]/div[3]/div/div[2]/div[1]/div/div[2]/div/div[3]')
-
-        # Use range() to add 2 ice creams using a loop
         for i in range(2):
-            self.driver.find_element(*ordering_ice_cream_button).click()
-
-        # Retrieve the count displayed
-        ice_cream_counter = (By.XPATH,
-                         "//div[contains(text(), 'Ice cream')]/..//div[contains(@class, 'counter')]")  # You need to find the correct locator
-        displayed_count = self.driver.find_element(*ice_cream_counter).text
-
-        # Assert that the displayed count matches 2
+            self.driver.find_element(*self.ordering_ice_cream_button).click()
+        displayed_count = self.driver.find_element(*self.ice_cream_counter).text
         assert displayed_count == "2"
 
 
-    def Final_taxi_order(self):
-        final_taxi_order_button = self.driver.find_element(By.XPATH, '//*[@id="root"]/div/div[3]/div[4]/button')
-        final_taxi_order_button.click()
-
-        # Wait for and assert that the car search modal appears
+    def final_taxi_order(self):
+        self.driver.find_element(*self.final_taxi_order_button).click()
         car_search_modal = WebDriverWait(self.driver, 10).until(
-        EC.visibility_of_element_located((By.CLASS_NAME, "order-body"))
+        ec.visibility_of_element_located((By.CLASS_NAME, "order-body"))
     )
         assert car_search_modal.is_displayed()
 
+    def active_selection_value(self):
+        return self.driver.find_element(*self.active_plan).text
